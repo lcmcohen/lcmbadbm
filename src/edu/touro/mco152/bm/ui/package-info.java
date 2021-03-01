1fb7a1efd7077e07e135023a4c41b9623ae9dd35
Eliyahu Steinhart <70113198+Steinhart365@users.noreply.github.com>
2021-03-01 00:31:17 -0500

    Update package-info.java
    I'm a bit unsure how to properly do the package-level commenting. I looked around online and did this based on what I picked up from there.

--- src/edu/touro/mco152/bm/ui/package-info.java
+++ src/edu/touro/mco152/bm/ui/package-info.java
@@ -1,1 +1,12 @@
+/**
+* Creates and controls the tangible aspects of our GUI, including the main GUI class, 
+* and additional classes for the MainFrame, SelectFrame, and RunPanel.
+*
+*Class Summary:
+*GUI.java
+*MainFrame.java
+*SelectFrame.java
+*RunPanel.java
+*/
+
 package edu.touro.mco152.bm.ui;
